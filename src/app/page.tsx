5f9dc976e11fa432b4afb03e5e4863e4c0bbca0d
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2024-12-19 18:23:28 -0800

    Remove placeholder text from homepage for a cleaner presentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
       <h1 className='text-4xl font-bold text-balance'>WELCOME TO KBQTECH-HUB</h1>
-      <p className='max-w-[40rem] text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita adipisci vitae repellendus facere voluptates fuga earum odit, itaque dolorem modi possimus ducimus. Eum dolor suscipit ratione, exercitationem assumenda nam et!</p>
     </main>
   );
 }
