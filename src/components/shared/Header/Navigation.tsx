63b12be4cfc74292a797c5614e1e933d5d56ba28
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-01-03 21:02:49 -0800

    Refactor Navigation component to use Link instead of anchor tags; enhance type safety with ComponentRef

--- src/components/shared/Header/Navigation.tsx
+++ src/components/shared/Header/Navigation.tsx
@@ -14,6 +14,7 @@ import {
     navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
 import { cn } from '@/lib/utils';
+import { ComponentRef } from 'react';
 
 const serviceLinks = [
     {
@@ -124,13 +125,13 @@ export default function Navigation() {
 }
 
 const ListItem = React.forwardRef<
-    React.ElementRef<'a'>,
-    React.ComponentPropsWithoutRef<'a'> & { icon?: React.ReactNode }
+    ComponentRef<typeof Link>,
+    React.ComponentPropsWithoutRef<typeof Link> & { icon?: React.ReactNode; title: string; children: React.ReactNode }
 >(({ className, title, children, icon, ...props }, ref) => {
     return (
         <li>
             <NavigationMenuLink asChild>
-                <a
+                <Link
                     ref={ref}
                     className={cn(
                         'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-pure-black hover:text-winny focus:bg-pure-black focus:text-winny text-yelly',
@@ -145,7 +146,7 @@ const ListItem = React.forwardRef<
                     <p className='line-clamp-2 text-sm leading-snug text-graphite-gray'>
                         {children}
                     </p>
-                </a>
+                </Link>
             </NavigationMenuLink>
         </li>
     );
