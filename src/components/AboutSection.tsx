e3fe5f3378f74986bc5cf816fc2f65ceca4843ff
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-01-08 23:19:48 -0800

    improved the about section title

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -3,13 +3,13 @@ import TextGradient from './shared/TextGradient'
 
 export default function AboutSection() {
     return (
-        <section className='bg-[#fff] shadow-2xl min-h-[500px] h-auto py-16 md:h-[60vh] md:min-h-[70vh] rounded-3xl relative flex flex-col items-center justify-center overflow-hidden px-4'>
+        <section className='bg-[#fff] shadow-2xl min-h-[400px] h-auto py-16 md:h-[60vh] md:min-h-[70vh] rounded-3xl relative flex flex-col items-center justify-center overflow-hidden px-4'>
             <div className='h-40 w-40 md:h-60 md:w-60 bg-gold rounded-full absolute -top-10 -right-10 md:-top-20 md:-right-20'></div>
             <div className='hidden md:flex size-[400px] absolute -top-50 left-0  -translate-x-60 circle_gradient rounded-full items-center justify-center'>
                 <div className='size-[380px] bg-[#fff] rounded-full'></div>
             </div>
             <div className='max-w-2xl mx-auto w-full z-10'>
-                <h2 className='text-xl sm:text-2xl md:text-3xl text-soft-black font-semibold text-center leading-normal'>
+                <h2 className='text-xl sm:text-3xl md:text-4xl text-soft-black font-semibold text-center leading-normal'>
                     On a mission to <TextGradient text='empower' /> businesses through <TextGradient text='innovative' /> technology, bridging gaps for <TextGradient text='profit' /> and <TextGradient text='simplified' /> efficient living.
                 </h2>
             </div>
