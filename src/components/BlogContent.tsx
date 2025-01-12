67e83f87dd449a3697507307302ba3b1d0314cf5
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2025-01-12 01:40:20 -0800

    Enhance BlogContent and BlogList components; add BlogCard for improved blog display and layout

--- src/components/BlogContent.tsx
+++ src/components/BlogContent.tsx
@@ -4,8 +4,8 @@ import BlogLlist from './BlogLlist'
 
 export default function BlogContent() {
     return (
-        <section className='min-h-screen bg-[#1F1E1E] rounded-b-[3rem] shadow-md'>
-            <div className='w-full max-w-5xl mx-auto'>
+        <section className='min-h-screen bg-[#1F1E1E] rounded-b-[3rem] shadow-md p-10'>
+            <div className='w-full max-w-6xl mx-auto'>
                 <BlogFilter />
                 <BlogLlist />
             </div>
