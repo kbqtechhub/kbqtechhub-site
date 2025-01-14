2a1ec4ca1319a4adf0ad1d0ceaba6f5194be3fb6
bamfokbq <keepthefaithkb@gmail.com>
2025-01-14 16:51:25 +0000

    Enhance NewsletterForm component; improve layout and styling for better user experience and engagement

--- src/components/NewsletterForm.tsx
+++ src/components/NewsletterForm.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function NewsletterForm() {
     return (
-        <div>NewsletterForm</div>
+        <section className="max-w-xl mx-auto w-full bg-gold rounded-3xl shadow-2xl p-10">
+            <h2 className="text-2xl font-bold text-center text-white">Subscribe to our newsletter</h2>
+            <p className="text-center text-white">Get the latest news, updates, and articles straight to your inbox.</p>
+            <form className="flex flex-col gap-4 mt-4">
+                <div className='flex gap-4 w-full'>
+                    <input type="email" placeholder="Email" className="p-2 rounded-lg flex-1" />
+                    <input type="text" placeholder="Name" className="p-2 rounded-lg flex-1" />
+                </div>
+                <div className='flex items-center justify-between gap-4'>
+                    <p className='text-white text-sm'>By submitting this form you are signing up for relevant content nd news form KBQ TECH-HUB. You can unsubscribe from these communication at any time.</p>
+                    <button className="bg-[#fff] text-yelly rounded-lg p-2">Subscribe</button>
+                </div>
+            </form>
+        </section>
     )
 }
